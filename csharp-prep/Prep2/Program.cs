9dcc8e6d71dcb7f0e80f442b10639c0ddd6c6689
KAYNALEDI <72366409+KAYNALEDI@users.noreply.github.com>
2023-04-23 17:31:56 -0400

    Finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,70 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+class Program {
+    static void Main(string[] args) {
+        // Core Requirements
+
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter;
+
+        if (grade >= 90) {
+            letter = "A";
+        }
+        else if (grade >= 80) {
+            letter = "B";
+        }
+        else if (grade >= 70) {
+            letter = "C";
+        }
+        else if (grade >= 60) {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+
+        Console.WriteLine("Your letter grade is: " + letter);
+
+        if (grade >= 70) {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else{
+            Console.WriteLine ("Better Luck next time!");
+        }
+
+        //Stretch
+         string sign;
+
+        if (letter == "A" && grade % 10 >= 7) {
+            sign = "+";
+        }
+        else if ((letter == "B" || letter == "C" || letter == "D") && grade % 10 >= 7) {
+            sign = "+";
+        }
+        else if (grade % 10 < 3 && letter != "F") {
+            sign = "-";
+        }
+        else {
+            sign = "";
+        }
+
+        if (letter == "A" && grade == 100) {
+            letter = "A+";
+        }
+        else if (letter == "F" && grade % 10 == 0) {
+            letter = "F";
+        }
+        else if (sign == "+") {
+            letter += sign;
+        }
+        else if (sign == "-") {
+            letter += sign;
+        }
+
+        Console.WriteLine("Your final grade is: " + letter);
     }
 }
+
+        
