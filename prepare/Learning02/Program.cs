1f30783e8bf7b7671868dc5e97f3aa46a8b09919
KAYNALEDI <72366409+KAYNALEDI@users.noreply.github.com>
2023-05-08 10:54:29 -0400

    updated code

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job("Z Modern", "Business Analyst", 2021, 2023);
+        Job job2 = new Job("Kunene Consulting", "Founder and CEO", 2022, 2023);
+
+        // Display the jobs
+        job1.Display();
+        job2.Display();
+
+        Resume myResume = new Resume("Nokwazi P Kunene");
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Display the resume
+        myResume.Display();
     }
 }
