fb7df13e5a929ebe235fd3841c33b1f9525689bd
KAYNALEDI <72366409+KAYNALEDI@users.noreply.github.com>
2023-05-14 16:07:02 -0400

    week5

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,54 @@
+using System;
+
+public class Fraction
+{
+    private int numerator;
+    private int denominator;
+
+    public Fraction()
+    {
+        numerator = 1;
+        denominator = 1;
+    }
+
+    public Fraction(int numerator)
+    {
+        this.numerator = numerator;
+        denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int Numerator
+    {
+        get { return numerator; }
+        set { numerator = value; }
+    }
+
+    public int Denominator
+    {
+        get { return denominator; }
+        set
+        {
+            if (value == 0)
+            {
+                throw new ArgumentException("Denominator cannot be zero.");
+            }
+            denominator = value;
+        }
+    }
+
+    public string GetFractionString()
+    {
+        return numerator + "/" + denominator;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)numerator / denominator;
+    }
+}
